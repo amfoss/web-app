089b4f49f382089fed5b94f9ee1c644f63d5fb67
harshithpabbati <pabbatiharshith@gmail.com>
2020-05-30 12:03:45 +0530

    pages: Update antd css file

--- pages/_app.js
+++ pages/_app.js
@@ -3,34 +3,7 @@ import React from 'react';
 import '../styles/styles.sass';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-quill/dist/quill.snow.css';
-
-// antd css files
-import 'antd/lib/card/style/index.css';
-import 'antd/lib/button/style/index.css';
-import 'antd/lib/input/style/index.css';
-import 'antd/lib/form/style/index.css';
-import 'antd/lib/breadcrumb/style/index.css';
-import 'antd/lib/layout/style/index.css';
-import 'antd/lib/menu/style/index.css';
-import 'antd/lib/drawer/style/index.css';
-import 'antd/lib/upload/style/index.css';
-import 'antd/lib/result/style/index.css';
-import 'antd/lib/checkbox/style/index.css';
-import 'antd/lib/alert/style/index.css';
-import 'antd/lib/tabs/style/index.css';
-import 'antd/lib/badge/style/index.css';
-import 'antd/lib/list/style/index.css';
-import 'antd/lib/avatar/style/index.css';
-import 'antd/lib/date-picker/style/index.css';
-import 'antd/lib/select/style/index.css';
-import 'antd/lib/switch/style/index.css';
-import 'antd/lib/tag/style/index.css';
-import 'antd/lib/table/style/index.css';
-import 'antd/lib/pagination/style/index.css';
-import 'antd/lib/page-header/style/index.css';
-import 'antd/lib/statistic/style/index.css';
-import 'antd/lib/collapse/style/index.css';
-import 'antd/lib/tag/style/index.css';
+import 'antd/dist/antd.min.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
