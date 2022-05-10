42b3d48c36028d729dbf98f52214a74bea77c617
Pavithra Nair <pmpavithranair@gmail.com>
2022-05-10 18:19:38 +0530

    fixed front-end

--- pages/bucketlist/view-list.js
+++ pages/bucketlist/view-list.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { marked } from "marked";
+import Cookies from 'universal-cookie';
+import dataFetch from '../../utils/dataFetch';
+
+const cookies = new Cookies();
 
 // antd components
 import Card from 'antd/lib/card';
-import Menu from 'antd/lib/menu';
 
 import Base from '../../components/base';
 import TitleBar from '../../components/titlebar';
@@ -22,14 +25,59 @@ const routes = [
     name: 'View Bucketlist',
   },
 ];
+
+const query = `
+query user($username: String!){
+  user(username:$username){
+    username
+    firstName
+    lastName
+    email
+    profile{
+      profilePic
+      phone
+      about
+      roll
+      batch
+      githubUsername
+      gitlabUsername
+      telegramUsername
+      twitterUsername
+      languages{
+        name
+      }
+      links{
+        link
+        portal{
+          name
+        }
+      }
+    }
+  }
+}
+`;
+
 const Viewlist = (props) => {
   const [data, setData] = useState([]);
+  const [batch, setBatch] = useState(0);
+  const [isLoaded, setLoaded] = useState(false);
+  const usernameCookie = cookies.get('username');
+  const variables = { username: usernameCookie };
+
+  const fetchData = async (variables) => dataFetch({ query, variables });
+
+  fetchData(variables).then((r) => {
+    if (!Object.prototype.hasOwnProperty.call(r, 'errors')) {
+      setBatch(r.data.user.profile.batch ? r.data.user.profile.batch : null);
+      setLoaded(true);
+    }
+  });
 
   useEffect(() => {
-    fetch('https://gitlab.com/api/v4/projects/20528933/repository/files/2020%2fadvaith.md/raw?private_token=glpat-TA_ZW_66kKtazaexHVCu&ref=master')
+    fetch(`https://gitlab.com/api/v4/projects/20528933/repository/files/${batch}%2f${usernameCookie}.md/raw?private_token=glpat-TA_ZW_66kKtazaexHVCu&ref=master`)
     .then(response => response.text())
     .then(data => setData(marked.parse(data)))
-  },[])
+  },[batch])
 
   return (
     <Base title="View list" {...props}>
@@ -37,7 +85,14 @@ const Viewlist = (props) => {
         routes={routes}
         title="View Bucketlist"
       />
-      <article dangerouslySetInnerHTML={{__html: data}}></article>
+        <div className="m-4" >
+          <Card
+            loading={!isLoaded}
+            type="inner"
+          >
+            <div dangerouslySetInnerHTML={{__html: data}}></div>
+          </Card>
+        </div>
     </Base>
   );
 };
